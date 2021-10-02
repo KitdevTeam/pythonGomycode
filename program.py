20860732a16610ac38a6812aa6ce2b3d7e04a6db
Nouha <43494970+nouha18@users.noreply.github.com>
2021-10-02 14:04:58 +0200

    commit python checkpoint

--- program.py
+++ program.py
@@ -6,8 +6,17 @@ def my_note(phrasal):
     for i in range(1,10,2):
         print("i want to learn"+phrasal)
 my_note(" React js")
-
+import numpy as np
 tuple1= (1,2,3)
 tuple2= (3,2,1)
 result = list(map(lambda x,y: x+y,tuple1,tuple2))
 print(result)
+A={5,12,1,23}
+tabs=np.array([3, 12,2])
+list={"abc",12,15,"c"}
+print(A)
+print(type(A))
+print(tabs)
+print(type(tabs))
+print(list)
+print(type(list))
---
+++ sumup.py
@@ -0,0 +1,10 @@
+###SumUp python courses
+list =[["list item 1", "list item2"],["list item3","list item4"]]
+    """type of list is list 
+
+# Creating a Nested Dictionary
+# as shown in the below image
+Dict = {1: 'Geeks', 2: 'For',
+        3:{'A' : 'Welcome', 'B' : 'To', 'C' : 'Geeks'}}
+    
+    """Array are the same as list except arrays accept only similar values however list accepts different values.
